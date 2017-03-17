04c15634fbe617721a4840b4c6c8f0bc76aa9f8d
Daniel Douglas Machado <danielprogramic@gmail.com>
2017-03-17 10:59:18 -0300

    diretiva modal

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,27 @@
     'use strict';
     var myApp = angular.module('MainAppDomRock', ['nya.bootstrap.select']);
 
+    myApp.directive('widowmodal', function() {
+        return {
+            template: '<div  class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">' +
+                '<div class="modal-dialog" role="document">' +
+                '<div class="modal-content">' +
+                '<div class="modal-header">' +
+                '<h5 class="modal-title" id="exampleModalLongTitle">{{sncards[id-1].title}}</h5>' +
+                '</div>' +
+                '<div class="modal-body">' +
+                '...' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
+                '<button type="button" class="btn btn-primary">Save changes</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+        };
+    });
+
     myApp.factory('optFactory', function($http) {
         var factory = [];
         factory.getTodos = function() {
@@ -14,7 +35,12 @@
         $scope.witingopts = [];
         $scope.monthopts = [];
         $scope.yearopts = [];
+        $scope.id = 0;
 
+        $scope.customer = {
+            name: 'Naomi',
+            address: '1600 Amphitheatre'
+        };
         for (var prop in typeselect) {
             $scope.witingopts.push(typeselect[prop].witing);
             $scope.monthopts.push(typeselect[prop].month);
@@ -26,10 +52,10 @@
         $scope.model3 = typeselect[2].year;
         $scope.sncards = cards;
 
-        $scope.clickmodal = function(taskId) {
-            alert("Task Id is " + taskId);
+        $scope.clickmodal = function(id) {
+            $scope.id = id;
+            $('#exampleModalLong').modal();
         };
-       $('#yourElement').fadeIn(700);
         // optFactory.getTodos().then(function(value) {
 
         //     for (var prop in value.data.typeselect) {
